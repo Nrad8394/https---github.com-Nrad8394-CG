0c07fd493702bf93ed83a4b1000cb8af39492fe8
Nrad8394 <94044917+Nrad8394@users.noreply.github.com>
2023-05-10 12:37:08 +0300

    commit3

--- base/models.py
+++ base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class items(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class transfer(models.Model):
+    item = models.ForeignKey(items, on_delete=models.CASCADE)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now_add=True)
+    transfer_status = models.CharField(max_length=100)
+    unique_code = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+
+    # def __str__(self):
+    #     return f"{self.item} - {self.transfer_status}"
+class delivery(models.Model):
+    code = models.ForeignKey()
+    
